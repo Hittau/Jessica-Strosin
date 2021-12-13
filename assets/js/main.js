533c1e328704d5a8214b6d949bef9c73cb0992e1
Hittau <74827598+Hittau@users.noreply.github.com>
2021-12-13 22:22:41 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 if (!localStorage.theme) localStorage.theme = "light";
 document.body.className = localStorage.theme
 
+document.querySelectorAll(".check").forEach(el => {
+    el.onchange = () => localStorage.setItem(el.id, el.checked);
+    el.checked = localStorage.getItem(el.id) === "true";
+});
+
 sliderThemeBtn.onclick = () => {
     document.body.classList.toggle('dark');
     localStorage.theme = document.body.className || "light";
